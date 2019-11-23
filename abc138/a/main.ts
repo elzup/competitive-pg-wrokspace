5909fb4693d45bf1281a60d39348ec0f5ae1a5e9
elzup <guild0105@gmail.com>
2019-11-23 21:00:28 +0900

    abca test

---
+++ abc138/a/main.ts
@@ -0,0 +1,24 @@
+{
+  const main = (input: string) => {
+    const r = reader(input)
+    const a = r.n()
+    const b = r.s()
+
+    console.log(a < 3200 ? 'red' : b)
+  }
+
+  // const values = obj => Object.keys(obj).map(e => obj[e]);
+
+  const reader = (str: string) => {
+    const lines = str.trim().split('\n')
+    const s = () => lines.shift() || ''
+    const line = s
+    const n = () => Number(s())
+
+    const ss = () => s().split(' ')
+    const ns = () => ss().map(Number)
+    return { lines, line, s, n, ss, ns }
+  }
+
+  main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+}
