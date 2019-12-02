6fe0fc1b46c42ddcba6cd55b54e5aa9aad0d5e2d
elzup <guild0105@gmail.com>
2019-12-03 08:45:58 +0900

    refactor

--- sumitrust2019/a/main.js
+++ sumitrust2019/a/main.js
@@ -4,25 +4,9 @@ const main = (input /*: string */) => {
   const r = reader(input)
   const [m1, d1] = r.ns()
   // const [m2, d2] = r.ns()
-  console.log(
-    Number(
-      {
-        [1]: 31,
-        [2]: 28,
-        [3]: 31,
-        [4]: 30,
-        [5]: 31,
-        [6]: 30,
-        [7]: 31,
-        [8]: 31,
-        [9]: 30,
-        [10]: 31,
-        [11]: 30,
-        [12]: 31,
-      }[m1] === d1
-    )
-  )
+  console.log(Number(lastDay(m1) === d1))
 }
+const lastDay = m => ({ '2': 28 }[m] || 30 + ((m >> 3) ^ (m & 1)))
 
 // const values = obj => Object.keys(obj).map(e => obj[e]);
 
