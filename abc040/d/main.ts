20f0527c1e216dca3f64f9057ccc84e66c2cf265
elzup <guild0105@gmail.com>
2019-12-19 19:25:27 +0900

    update unionfind

--- abc040/d/main.ts
+++ abc040/d/main.ts
@@ -4,7 +4,7 @@
     const [n, m] = r.ns()
     const aby: number[][] = []
 
-    const uf = new UnionFind(n)
+    const uf = _unionFind(n)
 
     for (let i = 0; i < m; i++) {
       const [a, b, y] = r.lines[i].split(' ').map(Number)
@@ -51,45 +51,31 @@
     return
   }
 
-  class UnionFind {
-    par: number[] = []
-    siz: number[] = []
+  const _unionFind = n => {
+    const par: number[] = Array.from(Array(n).keys())
+    const siz: number[] = Array(n).fill(1)
 
-    constructor(n: number) {
-      this.par = Array(n)
-      this.siz = Array(n).fill(1)
-      for (let i = 0; i < n; ++i) {
-        this.par[i] = i
-      }
-    }
-
-    root(x: number): number {
-      // 根の検索
-      while (this.par[x] != x) {
-        x = this.par[x] = this.par[this.par[x]]
+    // 根の検索
+    const root = (x: number): number => {
+      while (par[x] != x) {
+        x = par[x] = par[par[x]]
       }
       return x
     }
-
-    merge(x: number, y: number): boolean {
-      x = this.root(x)
-      y = this.root(y)
+    const merge = (x: number, y: number): boolean => {
+      x = root(x)
+      y = root(y)
       if (x == y) return false
-      if (this.siz[x] < this.siz[y]) [x, y] = [y, x]
+      if (siz[x] < siz[y]) [x, y] = [y, x]
       // merge technique（データ構造をマージするテク．小を大にくっつける）
-
-      this.siz[x] += this.siz[y]
-      this.par[y] = x
+      siz[x] += siz[y]
+      par[y] = x
       return true
     }
+    const issame = (x: number, y: number) => root(x) == root(y)
+    const size = (x: number) => siz[root(x)]
 
-    issame(x: number, y: number): boolean {
-      return this.root(x) == this.root(y)
-    }
-
-    size(x: number): number {
-      return this.siz[this.root(x)]
-    }
+    return { root, merge, issame, size }
   }
 
   const _reader = (str: string) => {
--- utils/tree.ts
+++ utils/tree.ts
@@ -1,40 +1,25 @@
-class UnionFind {
-  par: number[] = []
-  siz: number[] = []
+const _unionFind = n => {
+  const par: number[] = Array.from(Array(n).keys())
+  const siz: number[] = Array(n).fill(1)
 
-  constructor(n: number) {
-    this.par = Array(n)
-    this.siz = Array(n)
-    for (let i = 0; i < n; ++i) {
-      this.par[i] = i
-    }
-  }
+  // 根の検索
+  const root = (x: number): number => {
+    while (par[x] !== x) x = par[x] = par[par[x]]
 
-  root(x: number): number {
-    // 根の検索
-    while (this.par[x] != x) {
-      x = this.par[x] = this.par[this.par[x]]
-    }
     return x
   }
-
-  merge(x: number, y: number): boolean {
-    x = this.root(x)
-    y = this.root(y)
+  const merge = (x: number, y: number): boolean => {
+    x = root(x)
+    y = root(y)
     if (x == y) return false
-    if (this.siz[x] < this.siz[y]) [x, y] = [y, x]
+    if (siz[x] < siz[y]) [x, y] = [y, x]
     // merge technique（データ構造をマージするテク．小を大にくっつける）
-
-    this.siz[x] += this.siz[y]
-    this.par[y] = x
+    siz[x] += siz[y]
+    par[y] = x
     return true
   }
+  const issame = (x: number, y: number) => root(x) === root(y)
+  const size = (x: number) => siz[root(x)]
 
-  issame(x: number, y: number): boolean {
-    return this.root(x) == this.root(y)
-  }
-
-  size(x: number): number {
-    return this.siz[this.root(x)]
-  }
+  return { root, merge, issame, size }
 }
