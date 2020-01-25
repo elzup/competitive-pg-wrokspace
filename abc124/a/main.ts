285ddb0324fa4bec5a98dfaebf3b0f108bed073d
elzup <guild0105@gmail.com>
2020-01-25 17:30:42 +0900

    abc124

---
+++ abc124/a/main.ts
@@ -0,0 +1,31 @@
+{
+  const main = (
+    input: string
+  ): void | string | number | string[] | number[] => {
+    const r = _reader(input)
+    const [A, B] = r.nn()
+
+    return Math.max(A, B) * 2 - Number(A !== B)
+  }
+
+  const _reader = (str: string, i = 0) => {
+    const lines = str.trim().split('\n')
+    const s = () => lines[i++] || ''
+    const n = () => Number(s())
+
+    const mn = (v: string[]) => v.map(Number)
+    const sp = (v: string) => v.split(' ')
+    const ss = () => sp(s())
+    const nn = () => mn(ss())
+    const nls = () => mn(lines.slice(i))
+    const nnls = () => lines.slice(i).map(v => mn(sp(v)))
+
+    return { lines, s, n, ss, nn, nls, nnls }
+  }
+
+  const _getInput = () => require('fs').readFileSync('/dev/stdin', 'utf8')
+  const _res = main(_getInput())
+
+  if (typeof _res === 'number' || typeof _res === 'string') console.log(_res)
+  if (typeof _res === 'object') console.log(_res.join(' '))
+}
---
+++ abc124/c/main.ts
@@ -0,0 +1,42 @@
+{
+  const main = (
+    input: string
+  ): void | string | number | string[] | number[] => {
+    const r = _reader(input)
+    const S = r.s()
+
+    let a = 0
+    let b = 0
+
+    for (let i = 0; i < S.length; i++) {
+      if (S[i] === '01'[i % 2]) {
+        a++
+      } else {
+        b++
+      }
+    }
+
+    return Math.min(a, b)
+  }
+
+  const _reader = (str: string, i = 0) => {
+    const lines = str.trim().split('\n')
+    const s = () => lines[i++] || ''
+    const n = () => Number(s())
+
+    const mn = (v: string[]) => v.map(Number)
+    const sp = (v: string) => v.split(' ')
+    const ss = () => sp(s())
+    const nn = () => mn(ss())
+    const nls = () => mn(lines.slice(i))
+    const nnls = () => lines.slice(i).map(v => mn(sp(v)))
+
+    return { lines, s, n, ss, nn, nls, nnls }
+  }
+
+  const _getInput = () => require('fs').readFileSync('/dev/stdin', 'utf8')
+  const _res = main(_getInput())
+
+  if (typeof _res === 'number' || typeof _res === 'string') console.log(_res)
+  if (typeof _res === 'object') console.log(_res.join(' '))
+}
