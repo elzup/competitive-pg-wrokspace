840d10d745bbf4edb465d4756cbe7d6ae945b6e4
elzup <guild0105@gmail.com>
2020-01-31 21:14:14 +0900

    refactor ts utils

--- utils/util.ts
+++ utils/util.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /*
 links
 
@@ -5,46 +6,46 @@ https://github.com/samchon/tstl
  */
 
 // time year month day count
-const _lastDay = (m: number) => ({ '2': 28 }[m] || 30 + ((m >> 3) ^ (m & 1)))
+const lastDay = (m: number) => ({ '2': 28 }[m] || 30 + ((m >> 3) ^ (m & 1)))
 
 // leap year うるう年 閏年
-const _isReapYear = (y: number) =>
+const isReapYear = (y: number) =>
   (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0
 
 // rot shift
-const _rot = (s: string, n: number) =>
+const rot = (s: string, n: number) =>
   s
     .split('')
-    .map(c => _shift(c, n))
+    .map(c => shift(c, n))
     .join('')
 
-const _arr2D = (h: number, w: number, fill = 0) =>
+const arr2D = (h: number, w: number, fill = 0) =>
   new Array(h).fill(0).reduce(p => p.concat([Array(w).fill(fill)]), [])
 
-const _range = (n: number) => Array.from(Array(n).keys())
-const _product = (n: number, mod) =>
-  _range(n + 1).reduce((p, c) => (p * c) % mod, 1)
+const range = (n: number) => Array.from(Array(n).keys())
+const product = (n: number, mod) =>
+  range(n + 1).reduce((p, c) => (p * c) % mod, 1)
 
-const _chr = s => s.charCodeAt(0)
-const _ord = n => String.fromCharCode(n)
+const chr = (s: string) => s.charCodeAt(0)
+const ord = String.fromCharCode
 
-const _distance = (x1, y1, x2, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+const distance = (x1, y1, x2, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 // charcode shift
 const ac = 'A'.charCodeAt(0)
-const _shift = (c: string, n: number) =>
+const shift = (c: string, n: number) =>
   String.fromCharCode(((c.charCodeAt(0) + n - ac) % 26) + ac)
 
 // loop
-const _values = <T>(obj: { [k: string]: T }): T[] =>
+const values = <T>(obj: { [k: string]: T }): T[] =>
   Object.keys(obj).map(e => obj[e])
-const _map = <Val, T>(
+const map = <Val, T>(
   obj: Record<string, Val>,
   callback: (v: Val, k: string) => T
 ): T[] => Object.keys(obj).map(k => callback(obj[k], k))
 
 // colleciton
-const _groupBy = (arr, callback) =>
+const groupBy = (arr, callback) =>
   arr
     .map(v => [v, callback(v)])
     .reduce((obj, [v, k]) => {
@@ -54,9 +55,3 @@ const _groupBy = (arr, callback) =>
       obj[k].push(v)
       return obj
     }, {})
-
-// number of digit 10進数での 桁数
-const _nod = (n /*: number*/) => String(n).length
-
-const _comp = <T = string | number>(a: T, b: T) =>
-  a === b ? 0 : a > b ? 1 : -1
