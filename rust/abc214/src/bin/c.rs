497a3c5fa58d5d4afbc3971c8f079758490f217f
elzup <guild0105@gmail.com>
2021-08-21 21:09:31 +0900

    rust c

--- rust/abc214/src/bin/c.rs
+++ rust/abc214/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::*;
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s:[u64;n],
+        mut t:[u64;n],
+    }
+    for i in 0..(n * 2) {
+        let ni: usize = (i + 1) % n;
+        t[ni] = t[ni].min(t[i % n] + s[i % n]);
+    }
+    for v in t {
+        println!("{}", v);
+    }
 }
