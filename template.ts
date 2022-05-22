60f27eedd50565fba9b5b4f1932a7fa6e620b98c
elzup <guild0105@gmail.com>
2022-05-22 16:07:02 +0900

    update template

--- template.ts
+++ template.ts
@@ -1,39 +1,40 @@
 // import std = require('tstl')
 
-{
-  type Print = void | string | number | string[] | number[]
-  let outs: Print[] = []
+type Print = void | string | number | string[] | number[]
+let outs: Print[] = []
 
-  const main = (): Print => {
-    const r = _io()
-    const [N] = r.nn()
-    const [A, B] = r.nn()
+const main = (): Print => {
+  const r = _io()
+  const [N] = r.nn()
+  const [A, B] = r.nn()
 
-    return A + B + N
-  }
-
-  const _io = (i = 0) => {
-    const str = require('fs').readFileSync('/dev/stdin', 'utf8')
-    const lines = str.trim().split('\n')
-    const s = () => lines[i++] || ''
-    const n = () => Number(s())
-
-    const mn = (v: string[]) => v.map(Number)
-    const sp = (v: string) => v.split(' ')
-
-    const ss = () => sp(s())
-    const nn = () => mn(ss())
-    const nls = () => mn(lines.slice(i))
-    const nnls = () => lines.slice(i).map(v => mn(sp(v)))
-
-    return { lines, s, n, ss, nn, nls, nnls }
-  }
-
-  outs.push(main())
-
-  console.log(
-    outs
-      .map(line => (typeof line === 'object' ? line.join(' ') : String(line)))
-      .join('\n')
-  )
+  return A + B + N
 }
+
+const _io = (i = 0) => {
+  const str = require('fs').readFileSync('/dev/stdin', 'utf8')
+  const lines = str.trim().split('\n')
+  const s = () => lines[i++] || ''
+  const n = () => Number(s())
+
+  const mn = (v: string[]) => v.map(Number)
+  const sp = (v: string) => v.split(' ')
+
+  const ss = () => sp(s())
+  const nn = () => mn(ss())
+  const ls = () => lines.slice(i)
+  const nls = () => mn(ls())
+  const nnls = () => ls().map((v) => mn(sp(v)))
+
+  return { lines, s, n, ss, nn, ls, nls, nnls }
+}
+
+outs.push(main())
+
+console.log(
+  outs
+    .map((line) => (typeof line === 'object' ? line.join(' ') : String(line)))
+    .join('\n')
+)
+
+export default {}
--- typescript/utils/permutations.ts
+++ typescript/utils/permutations.ts
@@ -13,3 +13,25 @@ while (true) {
 console.log(x)
 
 export default {}
+
+function combination(nums, k) {
+  let ans: number[][] = []
+
+  if (nums.length < k) {
+    return []
+  }
+  if (k === 1) {
+    for (let i = 0; i < nums.length; i++) {
+      ans[i] = [nums[i]]
+    }
+  } else {
+    for (let i = 0; i < nums.length - k + 1; i++) {
+      let row = combination(nums.slice(i + 1), k - 1)
+
+      for (let j = 0; j < row.length; j++) {
+        ans.push([nums[i]].concat(row[j]))
+      }
+    }
+  }
+  return ans
+}
