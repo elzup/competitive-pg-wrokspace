24404659a07a5b119b94bc4d03110751eb9ef846
elzup <guild0105@gmail.com>
2022-05-22 16:44:19 +0900

    update

--- abc252/d/main.ts
+++ abc252/d/main.ts
@@ -10,34 +10,28 @@
     const A = r.nn()
 
     const t: Record<number, number> = {}
+    const c: Record<number, number> = {}
 
     A.forEach((i) => {
       t[i] = (t[i] || 0) + 1
     })
     let sum = 0
 
-    const u: Record<number, number> = {}
+    const MAX = 2 * 10 ** 5 + 1
 
-    Object.values(t).forEach((i) => {
-      u[i] = (u[i] || 0) + 1
-    })
+    c[-1] = 0
+    for (let i = 0; i < MAX; i++) {
+      c[i] = c[i - 1] + t[i] || 0
+    }
 
-    combination(Object.keys(u), 3).forEach((x) => {
-      // console.log(x)
+    Object.entries(t).forEach(([k, v]) => {
+      // console.log(k, v)
+      const d = c[Number(k) - 1]
 
-      sum += u[x[0]] * u[x[1]] * u[x[2]] * x[0] * x[1] * x[2]
-    })
-    combination(Object.keys(u), 2).forEach((x) => {
-      const x0 = x[0]
-      const x1 = x[1]
-      const rn0 = u[x0]
-      const rn1 = u[x1]
+      // console.log(d)
+      const u = N - c[k]
 
-      sum += rn0 * (rn0 - 1) * x0 * rn0 * x1
-      sum += rn1 * (rn1 - 1) * x1 * rn1 * x0
-    })
-    Object.entries(u).map(([k, v]) => {
-      sum += Number(k)
+      sum += d * u * v
     })
 
     // const l = Object.keys(t).length
@@ -68,51 +62,4 @@
       .map((line) => (typeof line === 'object' ? line.join(' ') : String(line)))
       .join('\n')
   )
-}
-
-function permutation(nums, k) {
-  let ans: number[][] = []
-
-  if (nums.length < k) {
-    return []
-  }
-  if (k === 1) {
-    for (let i = 0; i < nums.length; i++) {
-      ans[i] = [nums[i]]
-    }
-  } else {
-    for (let i = 0; i < nums.length; i++) {
-      let parts = nums.slice(0)
-
-      parts.splice(i, 1)[0]
-      let row = permutation(parts, k - 1)
-
-      for (let j = 0; j < row.length; j++) {
-        ans.push([nums[i]].concat(row[j]))
-      }
-    }
-  }
-  return ans
-}
-
-function combination(nums, k) {
-  let ans: number[][] = []
-
-  if (nums.length < k) {
-    return []
-  }
-  if (k === 1) {
-    for (let i = 0; i < nums.length; i++) {
-      ans[i] = [nums[i]]
-    }
-  } else {
-    for (let i = 0; i < nums.length - k + 1; i++) {
-      let row = combination(nums.slice(i + 1), k - 1)
-
-      for (let j = 0; j < row.length; j++) {
-        ans.push([nums[i]].concat(row[j]))
-      }
-    }
-  }
-  return ans
 }
